b62f66d31a382362997e840a527b84ff85a925e1
Yoo Hyung Joo <65493803+nacfson@users.noreply.github.com>
2023-06-03 23:24:31 +0900

    asdf

--- Assets/01.Scripts/Agent/AgentCamera.cs
+++ Assets/01.Scripts/Agent/AgentCamera.cs
@@ -6,23 +6,7 @@ using Photon.Pun;
 using Photon.Realtime;
 public class AgentCamera : MonoBehaviourPunCallbacks{    
 
-    public Vector3 finarDir;    
-    [SerializeField] private float _zoomSpeed;
-    [SerializeField] private float _maxOrthoSize = 15f;
 
-    [SerializeField] private float _followSpeed = 10f;
-    [SerializeField] private float _sensitivity = 30f;
-    [SerializeField] private float _clampAngle = 70f;
-
-    private float _rotX;
-    private float _rotY;
-
-    public Transform realCamera;
-    public Vector3 dirNormal;
-    public float maxDistance;
-    public float minDistance;
-    public float finalDistance;
-    
     private CinemachineVirtualCamera _followCam;
     private AgentInput _agentInput;
     private PhotonView _PV;
@@ -45,27 +29,15 @@ public class AgentCamera : MonoBehaviourPunCallbacks{
         //_agentInput.OnMouseScroll += OnScrollHandle;   
         _agentInput.OnMouseInput += OnMouseHandle; 
 
-        _rotX = _followCam.transform.localRotation.eulerAngles.x;
-        _rotY = _followCam.transform.localRotation.eulerAngles.y;
 
         string nickName = PhotonNetwork.LocalPlayer.NickName;
         CameraManager.Instance.AddCamera(this);
-    }
-    private void OnScrollHandle(float value){
-        _followCam.m_Lens.FieldOfView -= value * _zoomSpeed;
-        _followCam.m_Lens.FieldOfView = Mathf.Clamp(_followCam.m_Lens.FieldOfView,3f,_maxOrthoSize);
     }
 
     private void OnMouseHandle(float x, float y){
         if(_PV.IsMine == false) {
             return;
         }
-        _rotX += -y * _sensitivity * Time.deltaTime;
-        _rotY += x * _sensitivity * Time.deltaTime;
-
-        //_rotX = Mathf.Clamp(_rotX,0f,_clampAngle);
-        Quaternion rot = Quaternion.Euler(_rotX,_rotY,0);
-        _followCam.transform.rotation = rot;
     }
 
     public CinemachineVirtualCamera GetCamera(){
